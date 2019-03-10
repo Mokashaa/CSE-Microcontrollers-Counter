9f9174f0b2ac9f8330360df9bf31085721b5eeb5
Salma Tarek <1500667@eng.asu.edu.eg>
2019-03-10 22:49:07 +0200

    Added H files, C files, Assembly startup file, and Project file

---
+++ counter.h
@@ -0,0 +1,14 @@
+#ifndef COUNTER_H_
+#define COUNTER_H_
+
+#include "port.h"
+#include "DIO.h"
+
+// This function is used to increment numbers from 0 to 999 and print them to the LCD when the increment switch is pressed
+void increment ();
+// This function is used to decrement numbers from 999 to 0 and print them to the LCD when the decrement switch is pressed
+void decrement ();
+// This function is used to reset the counter to 0 and print it to the LCD screen
+void reset ();
+
+#endif
