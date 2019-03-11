1eeca633b15e529b96a12fb54f4ca426a68ac767
omarnagah <omarnagah4@gmail.com>
2019-03-11 21:57:38 +0200

    added Port_SetPinPullDown

--- port.c
+++ port.c
@@ -55,6 +55,35 @@ void Port_Init (uint8 port_index)
 	}
 }
 	
-
+void Port_SetPinPullDown(uint8 port_index,uint8 pins_mask,uint8 enable)
+{
+	switch(port_index)
+	{
+		case 'PA':
+			if (enable){GPIO_PORTA_PDR_R |= pins_mask;}
+			else {GPIO_PORTA_PDR_R &= ~pins_mask;}
+			break;
+		case 'PB':
+			if (enable){GPIO_PORTB_PDR_R |= pins_mask;}
+			else {GPIO_PORTB_PDR_R &= ~pins_mask;}
+			break;
+		case 'PC':
+			if (enable){GPIO_PORTC_PDR_R |= pins_mask;}
+			else {GPIO_PORTC_PDR_R &= ~pins_mask;}
+			break;
+		case 'PD':
+			if (enable){GPIO_PORTD_PDR_R |= pins_mask;}
+			else {GPIO_PORTD_PDR_R &= ~pins_mask;}
+			break;
+		case 'PE':
+			if (enable){GPIO_PORTE_PDR_R |= pins_mask;}
+			else {GPIO_PORTE_PDR_R &= ~pins_mask;}
+			break;
+		case 'PF':
+			if (enable){GPIO_PORTF_PDR_R |= pins_mask;}
+			else {GPIO_PORTF_PDR_R &= ~pins_mask;}
+			break;
+	}
+}
 
 	
