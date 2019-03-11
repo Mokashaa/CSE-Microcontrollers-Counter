5110f5726105fdc2d33de9b4577d516f20686aec
omarnagah <omarnagah4@gmail.com>
2019-03-11 21:39:17 +0200

    Added DIO_ReadPort

--- DIO.c
+++ DIO.c
@@ -1,1 +1,25 @@
 #include "DIO.h" 
+uint8 DIO_ReadPort (uint8 port_index, uint8 pins_mask)
+{
+	switch(port_index)
+	{
+	case 'PA':
+		return (GPIO_PORTA_DATA_R & pins_mask);
+	break;
+	case 'PB':
+	return (GPIO_PORTB_DATA_R & pins_mask);
+	break;
+	case 'PC':
+	return (GPIO_PORTC_DATA_R & pins_mask);
+	break;
+	case 'PD':
+	return (GPIO_PORTD_DATA_R & pins_mask);
+	break;
+	case 'PE':
+	return (GPIO_PORTE_DATA_R & pins_mask);
+	break;
+	case 'PF':
+	return (GPIO_PORTF_DATA_R & pins_mask);
+	break;
+	}
+}
