348c28e7f53320f0d056be4b2b4e2ccc76bb8367
omarnagah <omarnagah4@gmail.com>
2019-03-11 21:53:06 +0200

    added LCD_DisplayString, it includes conversion from int to char

--- lcd.c
+++ lcd.c
@@ -9,9 +9,22 @@ void LCD_SendData (uint8 data)
 
 }
 void LCD_DisplayString (uint16 i)
-{
-	
+{uint8 temp;
+	temp=(i/100)+48;// convert first digit to ascii
+	char x;
+	x=temp;
+	LCD_COMMAND(0x80);//first digit 
+	LCD_DATA(x);
+	temp=(temp/10)+48;// convert second digit to ascii
+	x=temp;
+	LCD_COMMAND(0x81);
+	LCD_DATA(x);
+	temp=(i%10)+48;	// convert third digit to ascii
+	x=temp;
+	LCD_COMMAND(0x82);
+	LCD_DATA(x);
 }
+
 void LCD_IntegerToString (uint16 data)
 {
 
