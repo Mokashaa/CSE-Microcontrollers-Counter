26754036085dd3589d2211ffdb12a016c28b012e
Salma Tarek <1500667@eng.asu.edu.eg>
2019-03-11 20:41:39 +0200

    Port_Init Function #7

--- port.c
+++ port.c
@@ -1,5 +1,60 @@
 #include "port.h"
-#include "std_types.h"
+
+void Port_Init (uint8 port_index)
+{
+	SYSCTL_RCGCGPIO_R |= (1 << port_index);
+	switch(port_index)
+	{
+		case PA:
+		{
+			GPIO_PORTA_LOCK_R = 0x4C4F434B;
+			GPIO_PORTA_CR_R = 0xFF;
+			GPIO_PORTA_DEN_R = 0xFF;
+			break;
+		}
+
+		case PB:
+		{
+			GPIO_PORTB_LOCK_R = 0x4C4F434B;
+			GPIO_PORTB_CR_R = 0xFF;
+			GPIO_PORTB_DEN_R = 0xFF;
+			break;
+		}
+
+		case PC:
+		{
+			GPIO_PORTC_LOCK_R = 0x4C4F434B;
+			GPIO_PORTC_CR_R = 0xFF;
+			GPIO_PORTC_DEN_R = 0xFF;
+			break;
+		}
+		
+		case PD:
+		{
+			GPIO_PORTD_LOCK_R = 0x4C4F434B;
+			GPIO_PORTD_CR_R = 0xFF;
+			GPIO_PORTD_DEN_R = 0xFF;
+			break;
+		}
+		
+		case PE:
+		{
+			GPIO_PORTE_LOCK_R = 0x4C4F434B;
+			GPIO_PORTE_CR_R = 0xFF;
+			GPIO_PORTE_DEN_R = 0xFF;
+			break;
+		}
+		
+		case PF:
+		{
+			GPIO_PORTF_LOCK_R = 0x4C4F434B;
+			GPIO_PORTF_CR_R = 0xFF;
+			GPIO_PORTF_DEN_R = 0xFF;
+			break;
+		}		
+	}
+}
+	
 
 
 	
