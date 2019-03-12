a6e136aa2baa4775069ba54d23f5663d491d6be7
Salma Tarek <1500667@eng.asu.edu.eg>
2019-03-12 16:09:08 +0200

    Update std_types.h

--- std_types.h
+++ std_types.h
@@ -5,7 +5,7 @@
 
 typedef unsigned char uint8;
 typedef unsigned short uint16;
-typedef unsigned long uint32;
+typedef unsigned long uint32_t;
 typedef unsigned long long uint64;
 typedef float float32;
 typedef double float64;
