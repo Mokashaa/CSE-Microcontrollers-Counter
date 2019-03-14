426df5175f203d7a87f31a5726d56d4387a34134
Mokashaa <mohamedokasha125@gmail.com>
2019-03-14 23:48:11 +0200

    Updating

--- dio.d
+++
@@ -1,5 +0,0 @@
-.\dio.o: DIO.c
-.\dio.o: DIO.h
-.\dio.o: tm4c123gh6pm.h
-.\dio.o: std_types.h
-.\dio.o: E:\keil\ARM\ARMCC\bin\..\include\stdio.h
--- main.d
+++
@@ -1,8 +0,0 @@
-.\main.o: main.c
-.\main.o: lcd.h
-.\main.o: port.h
-.\main.o: tm4c123gh6pm.h
-.\main.o: std_types.h
-.\main.o: E:\keil\ARM\ARMCC\bin\..\include\stdio.h
-.\main.o: DIO.h
-.\main.o: switch.h
