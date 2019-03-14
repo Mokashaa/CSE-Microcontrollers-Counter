adf1e58762cffdd1ac9fd585f6fe59916b8102a6
omarnagah <omarnagah4@gmail.com>
2019-03-14 18:49:09 +0200

    added increment

--- Application.c
+++ Application.c
@@ -2,7 +2,10 @@
 
 void increment (void)
 {
-	
+	i=i+1;
+	if (i>999)
+	{i=999;}
+	LCD_IntegerToString(i);
 }
 // This function is used to decrement numbers from 999 to 0 and print them to the LCD when the decrement switch is pressed
 void decrement (void)
