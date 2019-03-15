431f1495f11a25a30876ed59b749701219f5be2a
salma97 <1500667@eng.asu.edu.eg>
2019-03-15 19:39:35 +0200

    Fixing Delay

--- port.c
+++ port.c
@@ -2,13 +2,18 @@
 
 void Port_Init (uint8 port_index)
 {
+	int delay;
 	SYSCTL_RCGCGPIO_R |= (1 << port_index);
+	delay = SYSCTL_RCGCGPIO_R;
 	switch(port_index)
 	{
 		case PA:
 		{
 			GPIO_PORTA_LOCK_R = 0x4C4F434B;
 			GPIO_PORTA_CR_R = 0xFF;
+			GPIO_PORTA_AMSEL_R &= ~ 0xFF;
+			GPIO_PORTA_PCTL_R = 0x00000000;
+			GPIO_PORTA_AFSEL_R = 0x00;
 			GPIO_PORTA_DEN_R = 0xFF;
 			break;
 		}
@@ -17,6 +22,9 @@ void Port_Init (uint8 port_index)
 		{
 			GPIO_PORTB_LOCK_R = 0x4C4F434B;
 			GPIO_PORTB_CR_R = 0xFF;
+			GPIO_PORTB_AMSEL_R &= ~ 0xFF;
+			GPIO_PORTB_PCTL_R = 0x00000000;
+			GPIO_PORTB_AFSEL_R = 0x00;
 			GPIO_PORTB_DEN_R = 0xFF;
 			break;
 		}
@@ -25,6 +33,9 @@ void Port_Init (uint8 port_index)
 		{
 			GPIO_PORTC_LOCK_R = 0x4C4F434B;
 			GPIO_PORTC_CR_R = 0xFF;
+			GPIO_PORTC_AMSEL_R &= ~ 0xFF;
+			GPIO_PORTC_PCTL_R = 0x00000000;
+			GPIO_PORTC_AFSEL_R = 0x00;
 			GPIO_PORTC_DEN_R = 0xFF;
 			break;
 		}
@@ -33,6 +44,9 @@ void Port_Init (uint8 port_index)
 		{
 			GPIO_PORTD_LOCK_R = 0x4C4F434B;
 			GPIO_PORTD_CR_R = 0xFF;
+			GPIO_PORTD_AMSEL_R &= ~ 0xFF;
+			GPIO_PORTD_PCTL_R = 0x00000000;
+			GPIO_PORTD_AFSEL_R = 0x00;
 			GPIO_PORTD_DEN_R = 0xFF;
 			break;
 		}
@@ -41,6 +55,9 @@ void Port_Init (uint8 port_index)
 		{
 			GPIO_PORTE_LOCK_R = 0x4C4F434B;
 			GPIO_PORTE_CR_R = 0xFF;
+			GPIO_PORTE_AMSEL_R &= ~ 0xFF;
+			GPIO_PORTE_PCTL_R = 0x00000000;
+			GPIO_PORTE_AFSEL_R = 0x00;
 			GPIO_PORTE_DEN_R = 0xFF;
 			break;
 		}
@@ -49,6 +66,9 @@ void Port_Init (uint8 port_index)
 		{
 			GPIO_PORTF_LOCK_R = 0x4C4F434B;
 			GPIO_PORTF_CR_R = 0xFF;
+			GPIO_PORTF_AMSEL_R &= ~ 0xFF;
+			GPIO_PORTF_PCTL_R = 0x00000000;
+			GPIO_PORTF_AFSEL_R = 0x00;
 			GPIO_PORTF_DEN_R = 0xFF;
 			break;
 		}		
