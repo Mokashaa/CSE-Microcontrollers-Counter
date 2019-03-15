7f9a0d5009705ad6fa716e6468f249f61ef13ced
Mokashaa <mohamedokasha125@gmail.com>
2019-03-15 19:33:36 +0200

    Debuging

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
 #include "lcd.h"
 #include "switch.h"
 
-void SystemInit() {}
-	
-int main ()
+void Delay100ms(int x)
 {
-	Port_Init(PF);
-	Port_SetPinDirection(PF, 0x04, PORT_PIN_OUT);
-	while (1)
-	{
-		DIO_WritePort(PF, 0x04, STD_HIGH);
-		delay_ms(5000);
-	}
+unsigned long i;
+i = 1333333*x; 
+while(i > 0)
+i = i - 1;
 }
 
+void SystemInit() {
+	Port_Init(PF);
+	Port_SetPinDirection(PF, 0x04, PORT_PIN_OUT);
+	Port_SetPinPullUp(PF, 0x04, 1);
+  GPIO_PORTF_DATA_R |= 0x04;	
+}
+	
+int main(void){ unsigned long volatile delay;
+
+while(1)
+{
+DIO_WritePort(PF, 0x04, STD_LOW);
+
+Delay100ms(2); // delay for 100m
+
+	
+DIO_WritePort(PF, 0x04, STD_HIGH);
+	
+Delay100ms(2); // delay for 100ms
+
+
+} 
+}
